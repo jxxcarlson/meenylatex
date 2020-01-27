d806a92620dd374a740377eb75871b1eedf67626
James Carlson <jxxcarlson@mac.com>
2020-01-27 19:47:11 +0100

    add demo-package

---
+++ demo-package/src/Types.elm
@@ -0,0 +1,62 @@
+module Types exposing (..)
+
+import Internal.Parser exposing (LatexExpression)
+import MiniLatex.Edit
+import Html exposing (Html)
+import Time exposing (Posix)
+
+
+type alias Model a =
+    { counter : Int
+    , sourceText : String
+    , sourceText2 : String
+    , parseResult : List (List LatexExpression)
+    , inputString : String
+    , editData : MiniLatex.Edit.Data a
+    , version : Int
+    , configuration : Configuration
+    , lineViewStyle : LineViewStyle
+    , windowHeight : Int
+    , windowWidth : Int
+    , startTime : Posix
+    , stopTime : Posix
+    , message : String
+    }
+
+
+type Msg
+    = FastRender
+    | GetContent String
+    | ReRender
+    | Reset
+    | Restore
+    | GenerateSeed
+    | NewSeed Int
+    | ShowStandardView
+    | ShowParseResultsView
+    | ShowRawHtmlView
+    | ShowRenderToLatexView
+    | SetHorizontalView
+    | SetVerticalView
+    | TechReport
+    | WavePackets
+    | WeatherApp
+    | MathPaper
+    | Grammar
+    | Input String
+    | RequestStartTime
+    | RequestStopTime
+    | ReceiveStartTime Posix
+    | ReceiveStopTime Posix
+
+
+type LineViewStyle
+    = Horizontal
+    | Vertical
+
+
+type Configuration
+    = StandardView
+    | ParseResultsView
+    | RawHtmlView
+    | RenderToLatexView

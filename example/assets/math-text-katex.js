cdf7e1ec5b88f90d494621ca279c9dd0316770bf
James Carlson <jxxcarlson@gmail.com>
2020-07-08 14:06:32 +0200

    KaTeX experiment

--- example/assets/math-text-katex.js
+++ example/assets/math-text-katex.js
@@ -1,22 +1,21 @@
 class MathText extends HTMLElement {
 
+
    // The "set content" code below detects the
    // argument to the custom element
    // and is necessary for innerHTML
    // to receive the argument.
    set content(value) {
+        console.log('katex set content', value )
   		this.innerHTML = value
   	}
 
   connectedCallback() {
-    console.log(this.innerHTML)
+    console.log('katex connectedCallback',this.innerHTML )
     this.attachShadow({mode: "open"});
-    this.shadowRoot.innerHTML =
-     katex.render(this.innerHTML, {
-          throwOnError: false
-      });
-
-
+    this.shadowRoot.innerHTML = document.head.katexJs.renderToString("c = \\pm\\sqrt{a^2 + b^2}", {
+                                    throwOnError: false
+                                });
   }
 
 

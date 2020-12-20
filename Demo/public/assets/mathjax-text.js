f24f38a56974c406c2601ad7427b4fc78a9c8c16
James Carlson <jxxcarlson@gmail.com>
2020-12-19 19:01:27 -0500

    mathjax custom element with mathjax-text name

---
+++ Demo/public/assets/mathjax-text.js
@@ -0,0 +1,18 @@
+
+class MathJaxText extends HTMLElement {
+
+
+  connectedCallback() {
+      const content_ =
+        this.display
+          ? '$$' + this.content + '$$'
+          : '$' + this.content + '$' ;
+      this.attachShadow({mode: "open"});
+      this.shadowRoot.innerHTML =
+          '<mjx-doc><mjx-head></mjx-head><mjx-body>' + content_ + '</mjx-body></mjx-doc>';
+           MathJax.typesetShadow(this.shadowRoot)
+  }
+}
+
+customElements.define('mathjax-text', MathJaxText)
+
